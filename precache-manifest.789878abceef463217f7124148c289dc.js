8425ebd3fb223992df9579ae09b5fded9359ef4f
Álvaro <76259427+INPLAYSS@users.noreply.github.com>
2020-12-27 16:42:07 +0100

    Updates

---
+++ precache-manifest.789878abceef463217f7124148c289dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34627c4b0134b846adb037ef36b4cfbe",
+    "url": "/coronachinavirux/index.html"
+  },
+  {
+    "revision": "1f9dcb5d528773b99227",
+    "url": "/coronachinavirux/static/css/main.e3f054a2.chunk.css"
+  },
+  {
+    "revision": "8ce11feffdc405f1aa16",
+    "url": "/coronachinavirux/static/js/2.ef1cb14c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronachinavirux/static/js/2.ef1cb14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9dcb5d528773b99227",
+    "url": "/coronachinavirux/static/js/main.68fa5696.chunk.js"
+  },
+  {
+    "revision": "b545e663be3aac54af64",
+    "url": "/coronachinavirux/static/js/runtime-main.1c2e0b82.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/coronachinavirux/static/media/image.d7265326.png"
+  }
+]);
